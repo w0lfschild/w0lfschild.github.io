5854236479605ae40451277fd9c0780f6d25f25b
w0lfschild <aguywithlonghair@yahoo.com>
2018-08-23 12:20:06 -0700

    Bump
    - Fix loading paddle in pages that aren't selling anything
    - Added ico/favicon.png
    - Updated header and index page

--- Header.js
+++ Header.js
@@ -1,6 +1,4 @@
 document.write('\
-<header id="header-2" class="soft-scroll header-2">\
-</header>\
 <!DOCTYPE html> \
 <html lang="en" style="height:100%;">\
     <head> \
@@ -17,40 +15,35 @@ document.write('\
         <link href="css/plugins.css" rel="stylesheet">\
         <link href="css/blocks.css" rel="stylesheet">\
         <link href="css/custom.css" rel="stylesheet">\
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">\
         <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->         \
         <!--[if lt IE 9]>\
       <script src="js/html5shiv.js"></script>\
       <script src="js/respond.min.js"></script>\
     <![endif]-->         \
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">\
     </head>     \
-    <header id="custom_header">\
-        <nav class="main-nav navbar navbar-default navbar-fixed-top">\
-            <div class="container">\
-                <!-- Brand and toggle get grouped for better mobile display -->\
-                <!-- Collect the nav links, forms, and other content for toggling -->\
-                <!-- /.navbar-collapse -->\
+    <body data-spy="scroll" data-target="nav">\
+        <header id="header-2" class="soft-scroll header-2">\
+</header>\
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation"> \
+            <div class="container-fluid"> \
                 <div class="navbar-header">\
                     <button class="navbar-toggle" data-toggle="collapse" data-target="#nav1">\
                         <span class="sr-only">Toggle navigation</span> \
                         <span class="icon-bar"></span> \
                         <span class="icon-bar"></span> \
                         <span class="icon-bar"></span>\
-                    </button>\
-                </div>\
+                    </button>                     \
+                    <a href="#" class="navbar-brand finn"><i class="fas fa-terminal"></i> w0lfHub</a> \
+                </div>                 \
                 <div class="collapse navbar-collapse" id="nav1">\
-                    <ul class="nav navbar-nav navbar-left">\
-                        <li class="nav-item">\
-                            <a href="#" class="finn"><i class="fas fa-terminal"></i> w0lfHub</a>\
-                        </li>\
-                    </ul>\
-                    <ul class="nav navbar-nav navbar-right">\
-                        <li class="nav-item">\
+                    <ul class="nav navbar-nav navbar-right"> \
+                        <li>\
                             <a href="index.html"><i class="fas fa-home"></i> Home</a>\
-                        </li>\
-                        <li class="nav-item dropdown">\
-                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#"><i class="fab fa-app-store-ios"></i> Applications <i class="fas fa-angle-down"></i></a>\
-                            <ul class="dropdown-menu">\
+                        </li>                         \
+                        <li class="dropdown"> \
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fab fa-app-store-ios"></i> Applications <i class="fas fa-angle-down"></i></a> \
+                            <ul class="dropdown-menu"> \
                                 <li>\
                                     <a href="app_cDock.html"><i class="fas fa-paint-brush"></i> cDock</a>\
                                 </li>\
@@ -68,30 +61,31 @@ document.write('\
                                 </li>\
                                 <li>\
                                     <a href="app_vTool.html"><i class="fas fa-exchange-alt"></i> VPK Converter</a>\
-                                </li>\
-                            </ul>\
+                                </li>                                 \
+                            </ul>                             \
                         </li>\
-                        <!-- /.dropdown -->\
-                        <li class="nav-item dropdown">\
-                            <a class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" data-close-others="false" href="#"><i class="fas fa-cube"></i> Plugins <i class="fas fa-angle-down"></i></a>\
-                            <ul class="dropdown-menu">\
+                        <li class="dropdown"> \
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fas fa-cube"></i> Plugins <i class="fas fa-angle-down"></i></a> \
+                            <ul class="dropdown-menu"> \
                                 <li>\
                                     <a href="#">Coming soon...</a>\
-                                </li>\
-                            </ul>\
-                        </li>\
-                        <!-- /.dropdown -->\
-                        <li class="nav-item">\
+                                </li>                                 \
+                            </ul>                             \
+                        </li>                         \
+                        <li>\
                             <a href="mailto:aguywithlonghair@gmail.com?Subject=w0lfHub"><i class="fas fa-envelope"></i> Contact</a>\
                         </li>\
-                        <li class="nav-item">\
+                        <li>\
                             <a href="http://goo.gl/DSyEFR" class="bg-orange white"><i class="fab fa-paypal"></i> Donate</a>\
                         </li>\
                     </ul>\
                 </div>\
-            </div>\
-            <!-- /.container-fluid -->\
-        </nav>         \
-    </header>     \
+            </div>             \
+        </nav>\
+        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>         \
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>         \
+        <script type="text/javascript" src="js/plugins.js"></script>\
+        <script type="text/javascript" src="js/bskit-scripts.js"></script>         \
+    </body>     \
 </html>\
 ');
